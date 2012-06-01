3d0af69b300ad158a4fbd3f299fd8e72ca1803c6
gkats <giannis.katsanos@gmail.com>
2012-06-01 12:45:46 +0300

    Initial db data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Contact.create!(name: 'Peter Parker', group: 'family', 
+  address: '5th Avenue, Manhattan, New York, 20456', 
+  notes: 'With great power comes great responsibility')
+Contact.create!(name: 'Steve Rogers', group: 'family', 
+  address: '5th Avenue, Milan, Italy, 20456', notes: 'Stars and stripes')
+Contact.create!(name: 'Bruce Banner', group: 'friend', 
+  address: 'Nowhere, Middle, Italy, 20456', notes: 'Turns green when pissed off')
+Contact.create!(name: 'Tony Stark', group: 'friend', 
+  address: 'St Marine"s Cathedral, New York')
+Contact.create!(name: 'Logan', group: 'co-worker', 
+  address: 'St Marine"s Cathedral, New York', notes: 'No comments')
