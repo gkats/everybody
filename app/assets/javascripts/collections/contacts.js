64df7cfe16ffe70a2c0d934971a8fc4ecc886bce
gkats <giannis.katsanos@gmail.com>
2012-06-16 15:07:55 +0300

    Sort collection by name

--- app/assets/javascripts/collections/contacts.js
+++ app/assets/javascripts/collections/contacts.js
@@ -1,4 +1,7 @@
 Everybody.Collections.Contacts = Backbone.Collection.extend({
   url: '/api/contacts',
-  model: Everybody.Models.Contact
+  model: Everybody.Models.Contact,
+  comparator: function(contact) {
+    return contact.get('name');
+  }
 });
