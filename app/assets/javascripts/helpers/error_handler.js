88553329ca8c3241a04821fea9392ae5307d0405
gkats <giannis.katsanos@gmail.com>
2012-06-19 01:43:01 +0300

    Reused NotificationHandler

--- app/assets/javascripts/helpers/error_handler.js
+++ app/assets/javascripts/helpers/error_handler.js
@@ -1,24 +1,23 @@
-Everybody.Helpers.ErrorHandler = { };
-
-Everybody.Helpers.ErrorHandler.handleError = function(response) {
-  var notice = $('#notice');
-  // Client side validation
-  if (!response.status) {
-    $(notice).html(response);
-  }
-  // Server side validation
-  else if (response.status == 422) {
-    var errors = $.parseJSON(response.responseText).errors,
-      notices = '';
-    for (attribute in errors) {
-      var messages = errors[attribute];
-      for (var i = 0, len = messages.length; i < len; i++) {
-        message = messages[i];
-        attrName = "" + attribute;
-        notices += Everybody.Helpers.StringUtils.capitalize(
-          "" + attribute) + " " + message + " ";
-      }
+Everybody.Helpers.ErrorHandler = {
+  handleError: function(response) {
+    // Client side validation
+    if (!response.status) {
+      Everybody.Helpers.NotificationHandler.notify(response);
     }
-    $(notice).html(notices);
+    // Server side validation
+    else if (response.status == 422) {
+      var errors = $.parseJSON(response.responseText).errors,
+        notices = '';
+      for (attribute in errors) {
+        var messages = errors[attribute];
+        for (var i = 0, len = messages.length; i < len; i++) {
+          message = messages[i];
+          attrName = "" + attribute;
+          notices += Everybody.Helpers.StringUtils.capitalize(
+            "" + attribute) + " " + message + " ";
+        }
+      }
+      Everybody.Helpers.NotificationHandler.notify(notices);
+    }
   }
 };
