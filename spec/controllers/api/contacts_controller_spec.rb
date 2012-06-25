deb740c1676afd99120efbf40af823e6e8db5115
gkats <giannis.katsanos@gmail.com>
2012-06-25 23:05:58 +0300

    Removed unused specs

--- spec/controllers/api/contacts_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-describe ContactsController do
-  let(:contact) { FactoryGirl.create(:contact) }
-  
-  it 'should create a new contact' do
-    post '/contacts', contact.to_json
-    Contact.first.to_json.should have_json_path('id')
-  end
-end
