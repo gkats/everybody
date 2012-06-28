c0a39ca0098fb62212f727603a490852408018a9
Giannis Katsanos <giannis.katsanos@upstreamsystems.com>
2012-06-28 18:02:54 +0300

    Refactored toJSON method

--- app/assets/javascripts/models/contact.js
+++ app/assets/javascripts/models/contact.js
@@ -4,10 +4,25 @@ Everybody.Models.Contact = Backbone.Model.extend({
     address: '',
     notes: ''
   },
+
+	initialize: function() {
+		this.phones = new Everybody.Collections.Phones();
+	},
   
   validate: function(attrs) {
     if (Everybody.Helpers.ModelValidator.isBlank(attrs.name)) {
       return Everybody.Helpers.ModelValidator.messageForBlankValue('name');
     };
-  }
+  },
+
+	phones_attributes: function() {
+		return this.phones.map(function(p) {
+			return { type: p.get('type'), number: p.get('number') };
+		});
+	},
+	
+	toJSON: function() {
+		json = { contact: _.clone(this.attributes) };
+		return _.extend(json.contact, { phones_attributes: this.phones_attributes() });
+	}
 });
