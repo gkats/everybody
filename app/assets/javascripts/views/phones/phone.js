f35b9b813d17bac2e232a52296b3276af531f9f4
Giannis Katsanos <giannis.katsanos@upstreamsystems.com>
2012-07-02 12:35:48 +0300

    Phone fields removal allows to add another again

--- app/assets/javascripts/views/phones/phone.js
+++ app/assets/javascripts/views/phones/phone.js
@@ -2,7 +2,8 @@ Everybody.Views.Phone = Backbone.View.extend({
 	template: JST['phones/fields'],
 	
 	events: {
-	  'click .add-phone': 'addPhone'
+	  'click .add-phone': 'addPhone',
+		'click .remove-phone': 'removePhone'
 	},
 	
 	render: function() {
@@ -21,10 +22,30 @@ Everybody.Views.Phone = Backbone.View.extend({
 	
 	addPhone: function(e) {
 	  var fields;
+	
 	  e.preventDefault();
 	  e.stopPropagation();
-	  $(e.target).remove();
+	  $(e.target).toggle();
 	  fields = new Everybody.Views.Phone({ model: new Everybody.Models.Phone() });
-	  this.$el.append(fields.render().el);
+		fields.render().$el.append('<a href="" class="remove-phone">x</a>');
+	  this.$el.parent().append(fields.el);
+	},
+	
+	removePhone: function(e) {
+		var addLinks, phonesDiv, visibleLink = false;
+
+		e.preventDefault();
+		e.stopPropagation();
+		phonesDiv = $(e.target).parent('div').parent();
+		$(e.target).parent('div').remove();
+		// Make sure at least one add-phone link is displayed
+		$(phonesDiv).find('.add-phone').each(function(index, addLink) {
+			if (!$(addLink).attr('style').match(/display: none/)) {
+				visibleLink = true;
+			}
+		});
+		if (!visibleLink) {
+			$(phonesDiv).find('.add-phone:last').toggle();
+		}
 	}
 });
