23b2c624f6da16ba5e56aa35aad51770a0ea9348
gkats <giannis.katsanos@gmail.com>
2012-07-04 23:16:20 +0300

    Rewrite from scratch

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Everybody::Application.config.secret_token = '5c83578e625c34cb7ae0e93b1e4652d3f6e4042fb4400f9eff62efa1262743b38741481967ec25fc77f3306eff7866f656eb83fa9b71447829e57e7bd3c2ab3d'
