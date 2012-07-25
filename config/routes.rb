ede767502cbb5bc5f285f65bca22a2134c0ddcea
gkats <giannis.katsanos@gmail.com>
2012-07-25 21:36:05 +0300

    Setup for jasmine tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Everybody::Application.routes.draw do
-
   root to: 'main#index'
+  
+  if Rails.env.development? || Rails.env.test?
+    Rails.application.routes.draw do
+      mount Jasminerice::Engine => "/jasmine"
+    end
+  end
   
   match '*path', :to => 'main#index'
 end
